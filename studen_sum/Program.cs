ff0a6bb8673b1a720cd712c4287761a6fa6f3b0f
antonich k <antonich.nz@gmail.com>
2021-10-31 21:53:56 +1300

    finished

--- studen_sum/Program.cs
+++ studen_sum/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace studen_sum
+namespace StudentSummary
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] subjectNames = { "Math", "Phisics", "Chemistry" };
+            float[,] studentsResults = new float[3, 4] { { 87.6F, 89.1F, 93.0F, 78.5F }, { 71.2F, 65.4F, 88.6F, 75.0F }, { 42.8F, 93.5F, 68.4F, 78.2F } };
+            string yearResults = "";
+            char[] charsToTrim = { ',', ' ' };
+            float sum;
+            Console.WriteLine("End of the year transcript: ");
+            for (int i = 0; i < 3; i++)
+            {
+                yearResults = subjectNames[i] + ": ";
+                sum = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    yearResults += Convert.ToString(studentsResults[i, j]) + "%, ";
+                    sum += studentsResults[i, j];
+                }
+                yearResults = yearResults.TrimEnd(charsToTrim) + " average: " + Convert.ToString(sum / 4.0);
+                Console.WriteLine(yearResults);
+            }
         }
     }
 }
