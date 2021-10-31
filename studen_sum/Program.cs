6d41c686ebf9f8f0ea3855820e138763a3c562b8
antonich k <antonich.nz@gmail.com>
2021-10-31 21:52:41 +1300

    progress

---
+++ studen_sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace studen_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
